6cafb82708c0c017c890a7caf3fa46ab1f9f677f
PrakharV10 <70899048+PrakharV10@users.noreply.github.com>
2020-12-05 00:42:06 +0530

    Feat : Complete mobile functionality.

--- app.js
+++ app.js
@@ -13,9 +13,23 @@ const listenSub = document.getElementById("hp-sub-id");
 // Getting ready for the Speech Recognition
 const inputContent = document.querySelector("#input-content");
 const outputContent = document.querySelector("#output-content");
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;  
 
 const recognition = new SpeechRecognition();
+
+var url = "https://api.funtranslations.com/translate/yoda.json";
+var micClick = 0;
+var listenClick = 0;
+
+function createURL(transcript){
+    return url+ "?text=" + transcript;
+}
+
+function errorHandler(error){
+    console.log(error);
+    console.log("error occured");
+    alert("An error has occured, Please try again later!");
+}
 
 // Speech Recognition Begins.
 recognition.onstart = function(){
@@ -25,12 +39,25 @@ recognition.onstart = function(){
 recognition.onresult = function(event){
     var index = event.resultIndex;
     const transcript = event.results[index][0].transcript;
-    inputContent.textContent = transcript; 
+    inputContent.textContent = transcript;
+
+    // readOutLoud(transcript); 
+
+    fetch(createURL(transcript))
+    .then(response => response.json())
+    .then(json => {
+        var outputText = json.contents.translated;
+        outputContent.textContent = outputText;
+        
+    })
+    .catch(errorHandler);
+    
+    ListenBtn.addEventListener('click',function(){
+        readOutLoud(outputContent.innerHTML);
+    });
 }
 
 
-var micClick = 0;
-var listenClick = 0;
 
 
 // Changing the UI of the screen
@@ -47,24 +74,28 @@ function changeClass () {
     listenSub.className = "after-click";
     
     micClick += 1;
+    listenClick +=1;
 }
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
+
+
 
 // Adding function of Mic button click
-if(micClick == 0){
-    micBtn.addEventListener('click', function(){
-        setTimeout(changeClass,500);
-        recognition.start();
-    });
-}
-else{
-    micBtn.addEventListener('click', function(){
-        recognition.start();
-    });
-}
 
-// Adding function of speaker Button click
-if(listenClick == 0){
-    ListenBtn.addEventListener('click', function(){
-        alert('Speak something first');
-    });
-}
+micBtn.addEventListener('click', function(){
+    setTimeout(changeClass,500);
+    recognition.start();
+});
+
+
+
+
